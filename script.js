0405aaa6a19c1e1bb2d30b2b4a50910757eb4e55
Mhifee <104257100+Mhifee@users.noreply.github.com>
2022-06-25 16:43:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "Hannah Ojeyomi";
+const myHeight = "169cm";
+const myCountry = "Nigeria";
+
+alert("My name is " + myName + "\nMy height is " + myHeight + "\nI am from " + myCountry)
